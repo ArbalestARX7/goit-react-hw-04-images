f39f8b3de1ed558030d8c7fe6aaa64d4432ec3cd
Ikaruga666 <113863223+ArbalestARX7@users.noreply.github.com>
2023-06-11 23:57:06 +0300

    changed to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Notify, Report } from 'notiflix';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -8,126 +8,238 @@ import Button from 'components/Button/Button';
 import Loader from './Loader/Loader';
 import getPhotos from './api/API';
 
-class App extends Component {
-  state = {
-    querry: '',
-    page: 1,
-    largeImage: '',
-    showModal: false,
-    images: [],
-    error: '',
-    status: 'idle',
-    disabledBtn: true,
-    imagesAmount: '',
+const statusStages = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  REJECTED: 'rejected',
+  RESOLVED: 'resolved',
 };
 
-  async componentDidUpdate(prevProps, prevState) {
-    const oldQuerry = prevState.querry;
-    const newQuerry = this.state.querry;
+const { IDLE, PENDING, REJECTED, RESOLVED } = statusStages;
 
-    const oldPage = prevState.page;
-    const newPage = this.state.page;
+export default function App() {
+  const [querry, setQuerry] = useState('');
+  const [page, setPage] = useState(1);
+  const [largeImage, setLargeImage] = useState('');
+  const [showMoadal, setShowMoadal] = useState(false);
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState('');
+  const [status, setStatus] = useState(IDLE);
+  const [totalHits, SetTotalHits] = useState('');
 
-    if (oldQuerry !== newQuerry) {
-      if (newQuerry === '') {
-        return;
-      }
-
-      try {
-        this.setState({ status: 'pending' });
-        const newImages = await getPhotos(newQuerry, newPage);
-
-        if (newImages.hits.length < 12) {
-          this.setState({ disabledBtn: false });
-          } else{this.setState({disabledBtn: true})}
-
-        this.setState({
-          images: newImages.hits,
-          status: 'resolved',
-          imagesAmount: newImages.totalHits,
-        });
-      } catch (error) {
-        console.log(error);
-        this.setState({ status: 'rejected', error: error });
-      }
+  useEffect(() => {
+    if (querry === '') {
+      return;
     }
 
-    if (oldPage !== newPage && newPage !== 1) {
-      try {
-        const moreImages = await getPhotos(newQuerry, newPage);
-
-        if (moreImages.hits.length < 12) {
-          this.setState({ disabledBtn: false });
-          Notify.info('Sorry, this is the last images');
-        }
-
-        this.setState({
-          images: [...this.state.images, ...moreImages.hits],
-        });
-      } catch (error) {
-        console.log(error);
-        this.setState({ status: 'rejected', error: error });
-      }
+    if (page === 1) {
+      setStatus(PENDING);
+      getPhotos(querry, page)
+        .then(images => {
+          setImages(images.hits);
+          SetTotalHits(images.totalHits);
+          makeNeededNitifications(images);
+        })
+        .catch(error => {
+          setError(error);
+          setStatus(REJECTED);
+        })
+        .finally(setStatus(RESOLVED));
+      return;
     }
-  }
 
-  openModal = image => {
-    this.setState({ showModal: true, largeImage: image });
+    if (page > 1) {
+      setStatus(PENDING);
+      getPhotos(querry, page)
+        .then(images => {
+          setImages(prevImages => [...prevImages, ...images.hits]);
+          makeNeededNitifications(images);
+        })
+        .catch(error => {
+          setError(error);
+          setStatus(REJECTED);
+        })
+        .finally(setStatus(RESOLVED));
+    }
+  }, [page, querry]);
+
+  const makeNeededNitifications = images => {
+    if (images.hits.length < 12 && images.hits.length !== 0) {
+      Notify.info('Sorry, this is the last images');
+    }
+
+    if (images.hits.length === 0) {
+      Report.warning('Oops', 'There is no images by your request', 'OK');
+    }
   };
 
-  closeModal = () => {
-    this.setState({ showModal: false });
+  const openModal = image => {
+    setLargeImage(image);
+    setShowMoadal(true);
   };
 
-  findImagesByQuerry = newQuerry => {
-    this.setState({ querry: newQuerry, page: 1 });
+  const closeModal = () => {
+    setShowMoadal(false);
   };
 
-  onLoadMore = () => {
-    this.setState({ page: this.state.page + 1 });
+  const findImagesByQuerry = newQuerry => {
+    setPage(1);
+    setQuerry(newQuerry);
   };
 
-  render() {
-    const { largeImage, showModal, images, status, disabledBtn } =
-      this.state;
-    return (
-      <div className="App">
-        <Searchbar onSearch={this.findImagesByQuerry} />
+  const onLoadMore = () => {
+    setPage(page + 1);
+  };
 
-        {status === 'idle' && <h2>What are you searching for?</h2>}
+  return (
+    <div className="App">
+      <Searchbar onSearch={findImagesByQuerry} />
 
-        {status === 'pending' && <Loader />}
+      {status === IDLE && <h2>What are you searching for?</h2>}
 
-        {status === 'resolved' && (
-          <>
-            <ImageGallery showLargeImage={this.openModal} images={images} />
+      {status === PENDING && <Loader />}
 
-            {showModal && (
-              <Modal largeImage={largeImage} closeModal={this.closeModal} />
-            )}
+      {status === RESOLVED && (
+        <>
+          <ImageGallery showLargeImage={openModal} images={images} />
 
-            {disabledBtn && (
-              <Button onLoadMore={this.onLoadMore}  />
-            )}
-
-            {images.length === 0 &&
-              Report.warning(
-                'Oops',
-                'There is no images by your request',
-                'OK'
-              )}
-          </>
-        )}
-
-        {status === 'rejected' &&
-          Report.failure(
-            'Oops, somthing wrong:(',
-            'Please, try one more time.',
-            'Ok'
+          {showMoadal && (
+            <Modal largeImage={largeImage} closeModal={closeModal} />
           )}
-      </div>
-    );
-  }
+
+          {images.length !== totalHits && (
+            <Button onLoadMore={onLoadMore}></Button>
+          )}
+        </>
+      )}
+
+      {status === REJECTED &&
+        Report.failure(
+          'Oops, somthing wrong:(',
+          'Please, try one more time.',
+          'Ok'
+        )}
+    </div>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+//     querry: '',
+//     page: 1,
+//     largeImage: '',
+//     showModal: false,
+//     images: [],
+//     error: '',
+//     status: 'idle',
+//     disabledBtn: true,
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     const oldQuerry = prevState.querry;
+//     const newQuerry = this.state.querry;
+
+//     const oldPage = prevState.page;
+//     const newPage = this.state.page;
+
+//     if (oldQuerry !== newQuerry) {
+//       if (newQuerry === '') {
+//         return;
+//       }
+
+//       try {
+//         this.setState({ status: 'pending' });
+//         const newImages = await getPhotos(newQuerry, newPage);
+
+// if (newImages.hits.length < 12) {
+//   this.setState({ disabledBtn: false });
+// } else {
+//   this.setState({ disabledBtn: true });
+// }
+
+//         this.setState({
+//           images: newImages.hits,
+//           status: 'resolved',
+//         });
+//       } catch (error) {
+//         console.log(error);
+//         this.setState({ status: 'rejected', error: error });
+//       }
+//     }
+
+//     if (oldPage !== newPage && newPage !== 1) {
+//       try {
+//         const moreImages = await getPhotos(newQuerry, newPage);
+
+// if (moreImages.hits.length < 12) {
+//   this.setState({ disabledBtn: false });
+//   Notify.info('Sorry, this is the last images');
+// }
+
+//         this.setState({
+//           images: [...this.state.images, ...moreImages.hits],
+//         });
+//       } catch (error) {
+//         console.log(error);
+//         this.setState({ status: 'rejected', error: error });
+//       }
+//     }
+//   }
+
+//   openModal = image => {
+//     this.setState({ showModal: true, largeImage: image });
+//   };
+
+//   closeModal = () => {
+//     this.setState({ showModal: false });
+//   };
+
+//   findImagesByQuerry = newQuerry => {
+//     this.setState({ querry: newQuerry, page: 1 });
+//   };
+
+//   onLoadMore = () => {
+//     this.setState({ page: this.state.page + 1 });
+//   };
+
+//   render() {
+//     const { largeImage, showModal, images, status, disabledBtn } = this.state;
+//     return (
+//       <div className="App">
+//         <Searchbar onSearch={this.findImagesByQuerry} />
+
+//         {status === 'idle' && <h2>What are you searching for?</h2>}
+
+//         {status === 'pending' && <Loader />}
+
+//         {status === 'resolved' && (
+//           <>
+//             <ImageGallery showLargeImage={this.openModal} images={images} />
+
+//             {showModal && (
+//               <Modal largeImage={largeImage} closeModal={this.closeModal} />
+//             )}
+
+//             {disabledBtn && <Button onLoadMore={this.onLoadMore} />}
+
+// {images.length === 0 &&
+//   Report.warning(
+//     'Oops',
+//     'There is no images by your request',
+//     'OK'
+//   )}
+//           </>
+//         )}
+
+//         {status === 'rejected' &&
+//           Report.failure(
+//             'Oops, somthing wrong:(',
+//             'Please, try one more time.',
+//             'Ok'
+//           )}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,44 +1,76 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
+
+import PropTypes from 'prop-types';
 import css from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  state = {};
+function Modal({ largeImage, closeModal }) {
+  useEffect(() => {
+    window.addEventListener('keydown', evt => {
+      if (evt.code === 'Escape') {
+        closeModal();
+      }
+    });
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+    return () => {
+      window.addEventListener('keydown', evt => {
+        if (evt.code === 'Escape') {
+          closeModal();
+        }
+      });
+    };
+  }, [closeModal]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = evt => {
-    if (evt.code === 'Escape') {
-      this.props.closeModal();
-    }
-  };
-
-  handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={css.Overlay} onClick={this.handleBackdropClick}>
-        <div className={css.Modal}>
-          <img src={this.props.largeImage} alt="" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={css.Overlay} onClick={handleBackdropClick}>
+      <div className={css.Modal}>
+        <img src={largeImage} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = evt => {
+//     if (evt.code === 'Escape') {
+//       this.props.closeModal();
+//     }
+//   };
+
+//   handleBackdropClick = e => {
+//     if (e.target === e.currentTarget) {
+//       this.props.closeModal();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <div className={css.Overlay} onClick={this.handleBackdropClick}>
+//         <div className={css.Modal}>
+//           <img src={this.props.largeImage} alt="" />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
 
 export default Modal;
 
